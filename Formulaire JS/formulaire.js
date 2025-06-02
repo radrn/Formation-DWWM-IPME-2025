536995edbf4e39199b91cd3aad8594b4c7182621
Radia Drine <drineradiaa@gmail.com>
2025-06-02 14:41:40 +0200

    afficher input

--- Formulaire JS/formulaire.js
+++ Formulaire JS/formulaire.js
@@ -5,7 +5,6 @@ form.addEventListener('submit', (e) =>{
     e.preventDefault();
     
     let nameInput = document.querySelector("#name");
-    console.log(nameInput.value);
     let nomParagraphe = document.createElement("p");
     nomParagraphe.innerHTML = nameInput.value;
     div.append(nomParagraphe);
@@ -20,10 +19,27 @@ form.addEventListener('submit', (e) =>{
     ageParagraphe.innerHTML = ageInput.value + " ans";
     div.append(ageParagraphe);
 
+    let gender = document.querySelector("#man")
+
     let professionInput = document.querySelector("#profession");
     let professionOptions = document.createElement("p");
     professionOptions.innerHTML = professionInput.value;
     div.append(professionOptions);
 
-    
+    let regionInput = document.querySelector("#region");
+    let regionOptions = document.createElement("p");
+    regionOptions.innerHTML = regionInput.value;
+    div.append(regionOptions);
+
+    let checkboxInputs = document.querySelectorAll(".checkbox:checked");
+    let str = '';
+    for (let i = 0; i < checkboxInputs.length; i++) {
+        str += " " + checkboxInputs[i].value;
+    }
+    div.append(str);
+
+    let linkInput = document.querySelector("#link");
+    let linkPara = document.createElement("p");
+    linkPara.innerHTML = linkInput.value;
+    div.append(linkPara);
 });
